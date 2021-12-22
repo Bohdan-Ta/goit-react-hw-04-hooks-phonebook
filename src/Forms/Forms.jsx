7688103a0bbb99001c63b0e0700f10436759659a
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-12-22 12:07:08 +0200

    rewrite forms with useFormik

--- src/Forms/Forms.jsx
+++ src/Forms/Forms.jsx
@@ -1,66 +1,68 @@
-import { Component } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import React from "react";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 
 import s from "./Forms.module.css";
 
-const validationSchema = Yup.object({
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const RegistrationSchema = Yup.object().shape({
   name: Yup.string()
     .min(2, "Too Short!")
-    .matches(
-      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
-      "Name may contain only letters, apostrophe, dash and spaces. For example: Charles de Batz de Castelmore d'Artagnan"
-    )
-    .required("required"),
+    .max(20, "Too Long!")
+    .required("Required"),
   number: Yup.string()
-    .matches(
-      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
-      "Phone number is not valid"
-    )
-    .min(6)
-    .required("required"),
+    .matches(phoneRegExp, "invalid number")
+    .required("Required"),
 });
 
-export default class Forms extends Component {
-  render() {
-    return (
-      <div className={s.inputContainer}>
-        <Formik
-          initialValues={{ name: "", number: "" }}
-          validationSchema={validationSchema}
-          onSubmit={(values, { setSubmitting, resetForm }) => {
-            setSubmitting(false);
-            this.props.getSubmit(values);
-            resetForm();
-          }}
-        >
-          <Form>
-            <div className={s.inputContainer}>
-              <Field
-                type="text"
-                name="name"
-                className={s.input}
-                autoComplete="off"
-              />
-              <label className={s.label}>Name:</label>
-              <ErrorMessage name="name" component="div" className={s.error} />
-            </div>
-            <div className={s.inputContainer}>
-              <Field
-                type="tel"
-                name="number"
-                className={s.input}
-                autoComplete="off"
-              />
-              <label className={s.label}>Number:</label>
-              <ErrorMessage name="number" component="div" className={s.error} />
-            </div>
-            <button type="submit" className={s.borderButton}>
-              add contact
-            </button>
-          </Form>
-        </Formik>
-      </div>
-    );
-  }
+export default function Forms({ getSubmit }) {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      number: "",
+    },
+    validationSchema: RegistrationSchema,
+
+    onSubmit: (values, { resetForm }) => {
+      getSubmit(values);
+      resetForm();
+    },
+  });
+
+  return (
+    <div className={s.inputContainer}>
+      <form onSubmit={formik.handleSubmit}>
+        <div className={s.inputContainer}>
+          <input
+            type="text"
+            name="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+            className={s.input}
+            autoComplete="off"
+          />
+          {formik.errors.name}
+          <label htmlFor="name" className={s.label}>
+            Name:
+          </label>
+        </div>
+        <div className={s.inputContainer}>
+          <input
+            type="tel"
+            name="number"
+            onChange={formik.handleChange}
+            value={formik.values.number}
+            className={s.input}
+            autoComplete="off"
+          />
+          {formik.errors.number}
+          <label className={s.label}>Number:</label>
+        </div>
+        <button type="submit" className={s.borderButton}>
+          add contact
+        </button>
+      </form>
+    </div>
+  );
 }
